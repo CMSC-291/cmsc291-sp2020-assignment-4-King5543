639c3e877dd50c362850959cdd105a401641b15e
Austin King <aking8@umbc.edu>
2020-04-08 12:14:48 -0400

    first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\austin\PycharmProjects\cmsc291-sp2020-assignment-4-King5543\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
